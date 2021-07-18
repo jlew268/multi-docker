24055b86ade1f88c3d3d296127bde93fc802b4ea
jlew268 <jfizy@outlook.com>
2021-07-18 02:35:00 -0400

    force rebuild

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <header className="App-header">
+        <header className="App-head">
           <img src={logo} className="App-logo" alt="logo" />
           <a
             className="App-link"
